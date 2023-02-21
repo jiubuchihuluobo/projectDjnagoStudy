3f871491a673905312bf5ce80c01ba965579f279
jiubuchihuluobo <date20150619@icloud.com>
2023-02-21 21:40:06 +0800

    添加contentype

---
+++ customauth/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class TaggedItem(models.Model):
+    tag = models.SlugField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+
+
+class User(AbstractUser):
+    tags = GenericRelation(TaggedItem, related_query_name="user")
+
+
+class Video(models.Model):
+    name = models.CharField(max_length=64)
