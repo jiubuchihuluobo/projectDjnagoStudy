e5a893477af68fc3d6711817b3fa6b93ba4e1e7d
jiubuchihuluobo <date20150619@icloud.com>
2023-02-27 20:25:37 +0800

    添加标签模型类

--- customauth/models.py
+++ customauth/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
@@ -19,3 +20,16 @@ class Task(models.Model):
 
     class Meta:
         ordering = ['completed']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64, name="标签名字")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # 正数或0
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey(ct_field="content_type", fk_field="object_id")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
