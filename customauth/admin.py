b9a262efa177d5fa2ece846d0fa8f8566fbf49d0
jiubuchihuluobo <date20150619@icloud.com>
2023-02-28 20:11:13 +0800

    移动task模型类位置

--- customauth/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from customauth.models import Task
-
-admin.site.register(Task)
--- customauth/models.py
+++ customauth/models.py
@@ -8,20 +8,6 @@ class User(AbstractUser):
     ...
 
 
-class Task(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(null=True, blank=True)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ['completed']
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=64, name="标签名字")
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
