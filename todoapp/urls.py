59ea41541382b09daefc356e163143c3577b89f0
jiubuchihuluobo <date20150619@icloud.com>
2023-02-28 22:32:09 +0800

    表单类的使用

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from todoapp.views import HomeView
+from todoapp.views import HomeView, TaskView, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 app_name = "todoapp"
 
 urlpatterns = [
-    path("home/", HomeView.as_view(), name="home")
+    path("home/", HomeView.as_view(), name="home"),
+    path("list/", TaskView.as_view(), name="tasks"),
+    path('list/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('list/create/', TaskCreate.as_view(), name='create'),
+    path('list/update/<int:pk>/', TaskUpdate.as_view(), name='update'),
+    path('list/delete/<int:pk>/', TaskDelete.as_view(), name='delete'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,55 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from todoapp.models import Task
 
 
 class HomeView(View):
     def get(self, request):
         return render(request, "todoapp/home.html")
+
+
+class TaskView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+
+
+class TaskCreate(CreateView):
+    # 指定要创建对象的类
+    model = Task
+    # 指定表单上显示的字段
+    fields = ['title', 'description', 'completed']
+    # 是任务成功创建后Django将重定向到的目标URL
+    success_url = reverse_lazy('todoapp:tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "The task was created successfully.")
+        # 如果验证成功保存并重定向到success_url
+        return super(TaskCreate, self).form_valid(form)
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('todoapp:tasks')
+    # 默认与CreateView公用一个视图，可以不设置
+    template_name = "todoapp/task_form.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was updated successfully.")
+        return super(TaskUpdate, self).form_valid(form)
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('todoapp:tasks')
