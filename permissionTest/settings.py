31454f6f5edf04d8b6cd9eab6603c224267773aa
jiubuchihuluobo <date20150619@icloud.com>
2023-03-02 16:45:54 +0800

    添加新的应用

--- permissionTest/settings.py
+++ permissionTest/settings.py
@@ -35,9 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_extensions",
     "customauth",
     "demo",
-    "todoapp"
+    "todoapp",
+    "hr",
 ]
 
 MIDDLEWARE = [
