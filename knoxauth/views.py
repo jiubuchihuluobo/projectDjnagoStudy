e71fdd42c5b80a612e6dbeb4942b12b851eb303a
jiubuchihuluobo <date20150619@icloud.com>
2023-03-07 22:13:45 +0800

    loginview添加新的验证器

--- knoxauth/views.py
+++ knoxauth/views.py
@@ -1,5 +1,6 @@
 from knox.views import LoginView
+from rest_framework.authentication import BasicAuthentication
 
 
 class KnoxLoginView(LoginView):
-    ...
+    authentication_classes = (BasicAuthentication,)
