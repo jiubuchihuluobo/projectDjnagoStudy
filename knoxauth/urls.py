c4acd8b971ddfdd988428547869c69f248e45b11
jiubuchihuluobo <date20150619@icloud.com>
2023-03-08 16:34:47 +0800

    添加全部退出的路由

--- knoxauth/urls.py
+++ knoxauth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from knoxauth.views import KnoxLoginView, KnoxLogoutView
+from knoxauth.views import KnoxLoginView, KnoxLogoutView, KnoxLogoutALLView
 
 app_name = "knox"
 
 urlpatterns = [
     path("login/", KnoxLoginView.as_view(), name="login"),
     path("logout/", KnoxLogoutView.as_view(), name="logout"),
+    path("logoutall/", KnoxLogoutALLView.as_view(), name="logoutall")
 ]
